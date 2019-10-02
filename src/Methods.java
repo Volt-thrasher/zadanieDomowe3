ba2797ea886fe1b021e01628aa461a898da7d88d
Volt-thrasher <55741371+Volt-thrasher@users.noreply.github.com>
2019-10-02 17:02:50 +0200

    Add files via upload
    Methods created and tested

---
+++ src/Methods.java
@@ -0,0 +1,18 @@
+public class Methods {
+    boolean isEven(int a) {
+        boolean even = a % 2 == 0;
+        return even;
+    }
+    boolean isOdd(int a){
+        boolean odd = a%2 == 1;
+        return odd;
+    }
+    double circleField(double r){
+       double fld = r*r*3.14;
+        return fld;
+    }
+    int power(int a){
+        int pwr = a*a;
+        return pwr;
+    }
+}
